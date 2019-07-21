dea072ebc352b4bfd30b9c80e335400be7f5a878
FranSollima <francisco@sollima.net>
2019-07-21 18:38:55 -0300

    Mejoras

--- pokemon_ev_assistant.js
+++ pokemon_ev_assistant.js
@@ -1,10 +1,14 @@
-function update_colors(){
+function get_ev_multiplier(){
 	var macho_brace = document.getElementById('macho_brace_checkbox').checked;
-	var macho_brace_mult = 1;
-	if(macho_brace) {
-		macho_brace_mult = 2;
-	};
+	var horde = document.getElementById('horde_checkbox').checked;
+	var ev_multiplier = 1;
+	if(macho_brace) {ev_multiplier = ev_multiplier*2;};
+	if(horde) {ev_multiplier = ev_multiplier*5;};
+	return ev_multiplier
+}
+function update_colors(){
 	var pokemon_buttons = document.getElementsByClassName('pokemon_button');
+	var ev_multiplier = get_ev_multiplier();
 	for (var i = pokemon_buttons.length - 1; i >= 0; i--) {
 		//Missing EVs
 		var hp_missing = document.getElementById('hp_obj').value-document.getElementById('hp_act').value;
@@ -14,22 +18,22 @@ function update_colors(){
 		var spd_missing = document.getElementById('spd_obj').value-document.getElementById('spd_act').value;
 		var spe_missing = document.getElementById('spe_obj').value-document.getElementById('spe_act').value;
 
-		if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('hp')) > hp_missing){
+		if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('hp')) > hp_missing){
 			//HP
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
-		} else if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('atk')) > atk_missing){
+		} else if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('atk')) > atk_missing){
 			//ATK
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
-		} else if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('def')) > def_missing){
+		} else if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('def')) > def_missing){
 			//DEF
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
-		} else if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('spa')) > spa_missing){
+		} else if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('spa')) > spa_missing){
 			//SPA
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
-		} else if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('spd')) > spd_missing){
+		} else if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('spd')) > spd_missing){
 			//SPD
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
-		} else if(macho_brace_mult*parseInt(pokemon_buttons[i].getAttribute('spe')) > spe_missing){
+		} else if(ev_multiplier*parseInt(pokemon_buttons[i].getAttribute('spe')) > spe_missing){
 			//SPE
 			pokemon_buttons[i].style.backgroundColor = 'indianred';
 		} else {
@@ -38,7 +42,7 @@ function update_colors(){
 	}
 };
 function display_pokemon(){
-	document.getElementById(document.getElementById('pokemon_list').value).style.display='block';
+	document.getElementsByName(document.getElementById('pokemon_list').value)[0].style.display='block';
 	document.getElementById('pokemon_list').value = '';
 };
 function complete_evs(){
@@ -55,20 +59,16 @@ function complete_evs(){
 	if(!document.getElementById('spd_obj').value){ document.getElementById('spd_obj').value = "0"};
 	if(!document.getElementById('spe_obj').value){ document.getElementById('spe_obj').value = "0"};
 }
-function add_effort_values(pokemon_id){
-	var macho_brace = document.getElementById('macho_brace_checkbox').checked;
-	var macho_brace_mult = 1;
-	if(macho_brace) {
-		macho_brace_mult = 2;
-	};
-	if(confirm("Add EVs from "+pokemon_id+"?")){
+function add_effort_values(pokemon_id, pokemon_name){
+	var ev_multiplier = get_ev_multiplier();
+	if(confirm("Add EVs from "+pokemon_name+"?")){
 		complete_evs();
-		document.getElementById('hp_act').value = parseInt(document.getElementById('hp_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('hp'));
-		document.getElementById('atk_act').value = parseInt(document.getElementById('atk_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('atk'));
-		document.getElementById('def_act').value = parseInt(document.getElementById('def_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('def'));
-		document.getElementById('spa_act').value = parseInt(document.getElementById('spa_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('spa'));
-		document.getElementById('spd_act').value = parseInt(document.getElementById('spd_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('spd'));
-		document.getElementById('spe_act').value = parseInt(document.getElementById('spe_act').value) + macho_brace_mult*parseInt(document.getElementById(pokemon_id).getAttribute('spe'));
+		document.getElementById('hp_act').value = parseInt(document.getElementById('hp_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('hp'));
+		document.getElementById('atk_act').value = parseInt(document.getElementById('atk_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('atk'));
+		document.getElementById('def_act').value = parseInt(document.getElementById('def_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('def'));
+		document.getElementById('spa_act').value = parseInt(document.getElementById('spa_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('spa'));
+		document.getElementById('spd_act').value = parseInt(document.getElementById('spd_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('spd'));
+		document.getElementById('spe_act').value = parseInt(document.getElementById('spe_act').value) + ev_multiplier*parseInt(document.getElementById(pokemon_id).getAttribute('spe'));
 		update_colors();
 	}
 };
